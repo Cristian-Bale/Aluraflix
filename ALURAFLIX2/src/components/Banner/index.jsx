7e46165c41143e4403cfe4c270a06ff745a216a9
Cristian <cristianduran187@gmail.com>
2024-07-17 00:22:27 -0300

    Cuarto commit

--- ALURAFLIX2/src/components/Banner/index.jsx
+++ ALURAFLIX2/src/components/Banner/index.jsx
@@ -1,26 +1,25 @@
+import React from "react";
 import styled from "styled-components";
-import BannerImageSrc from "../../assets/Banner.png"; 
-
+import BannerImageSrc from "../../assets/banner.png";
 const BannerContainer = styled.div`
-    background-image: url(${BannerImageSrc}); 
-    background-repeat: no-repeat;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    min-height: 328px;
-    margin: 0;
-    border-radius: 20px;
-    max-width: 100%;
-    height: 100vh;
-    background-size: cover;
+  background-image: url(${BannerImageSrc});
+  background-repeat: no-repeat;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 328px;
+  margin: 0;
+  border-radius: 20px;
+  max-width: 100%;
+  height: 100vh;
+  background-size: cover;
 `;
 
 const Banner = () => {
-    return (
-        <BannerContainer>
-          
-        </BannerContainer>
-    );
-}
+  return (
+    <BannerContainer>
+    </BannerContainer>
+  );
+};
 
 export default Banner;
